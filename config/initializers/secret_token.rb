e1c45ef9181bfa95b87b50760ccd4268c6b602bd
Travis Brace <TravisBrace@gmail.com>
2014-07-08 02:19:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnydermanWorksGalleries::Application.config.secret_token = 'fb84cbb942ba2d40a514ca1e38134839329431f27c69616b27233d1aace45eed3ef316644c7b246bac85dbe1a3b72f071b967ec7616e73069d56c01096f300a2'
